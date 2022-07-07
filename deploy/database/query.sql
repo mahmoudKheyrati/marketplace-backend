cb2a518d7544a85e0b39fe2fea32c47df14178af
mahmoud reza kheyrati fard <64142359+mahmoudKheyrati@users.noreply.github.com>
2022-07-07 15:14:26 +0430

    add crud for store

--- deploy/database/query.sql
+++ deploy/database/query.sql
@@ -61,11 +61,11 @@ select name, expected_arrival_working_days, base_cost, created_at
 from shipping_method;
 
 -- category
-insert into category( id,  name, parent)
+insert into category(id, name, parent)
 values (1, 'electronics', null),
        (2, 'laptop', 1),
        (3, 'phone', 1),
-       (4, 'monitor' , 1),
+       (4, 'monitor', 1),
        (5, 'shoes', null),
        (6, 'men-shoes', 5),
        (7, 'boot', 6),
@@ -76,11 +76,42 @@ values (1, 'electronics', null),
        (12, 'flat', 9);
 
 -- get main categories
-select name from category where parent is null;
+select name
+from category
+where parent is null;
 -- get sub-categories by category id
-select name from category where parent = ? ;
+select name
+from category
+where parent = ?;
 -- get all categories with sub-categories
 -- todo: implement later if needed.
+
+-- create store
+insert into store(name, description, avatar_url, owner, creator)
+values (?, ?, ?, ?, ?);
+
+-- update store
+update store
+set name= ?,
+    description = ?,
+    avatar_url = ?,
+    owner = ?
+where id = ?;
+
+-- get user all stores
+select id, name, description, avatar_url, owner, creator, created_at
+from store
+where creator = ?;
+
+-- get all marketplace stores
+select id, name, description, avatar_url, owner, creator, created_at
+from store;
+-- delete store
+delete
+from store
+where id = ?
+  and creator = ?;
+
 
 
 
