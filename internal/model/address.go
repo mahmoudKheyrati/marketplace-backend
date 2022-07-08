ad48c26d1ab3feb6a316402af29084eb0e718e06
mahmoud reza kheyrati fard <64142359+mahmoudKheyrati@users.noreply.github.com>
2022-07-08 21:12:58 +0430

    add json tag to address model

--- internal/model/address.go
+++ internal/model/address.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type Address struct {
-	Id              int64
-	UserId          int64
-	Country         string
-	Province        string
-	City            string
-	Street          string
-	PostalCode      string
-	HomePhoneNumber string
-	CreatedAt       time.Time
+	Id              int64     `json:"id"`
+	UserId          int64     `json:"user_id"`
+	Country         string    `json:"country"`
+	Province        string    `json:"province"`
+	City            string    `json:"city"`
+	Street          string    `json:"street"`
+	PostalCode      string    `json:"postal_code"`
+	HomePhoneNumber string    `json:"home_phone_number"`
+	CreatedAt       time.Time `json:"created_at"`
 }
