8ab96f8989d7cd21819f9937db655fd854debbc7
mahmoud reza kheyrati fard <64142359+mahmoudKheyrati@users.noreply.github.com>
2022-07-08 10:48:43 +0430

    move user-permission insert query into init.sql

--- deploy/database/init.sql
+++ deploy/database/init.sql
@@ -16,6 +16,15 @@ create table user_permission
     created_at                   timestamptz default now()
 );
 
+insert into user_permission(name, is_system_admin, system_read_access, system_edit_access_product,
+                            system_create_access_product, system_create_new_store, store_create_new_product,
+                            store_read_new_product, store_edit_new_product, is_employee)
+values ('normal-user', false, false, false, false, false, false, false, false, false),
+       ('store-admin', false, false, false, false, false, true, true, true, false),
+       ('marketplace-admin', true, true, true, true, true, false, false, false, true);
+
+
+
 drop table if exists "user" cascade;
 create table "user" -- login with email and password
 (
--- deploy/database/query.sql
+++ deploy/database/query.sql
@@ -1,10 +1,3 @@
--- user permissions
-insert into user_permission(name, is_system_admin, system_read_access, system_edit_access_product,
-                            system_create_access_product, system_create_new_store, store_create_new_product,
-                            store_read_new_product, store_edit_new_product, is_employee)
-values ('normal-user', false, false, false, false, false, false, false, false, false),
-       ('store-admin', false, false, false, false, false, true, true, true, false),
-       ('marketplace-admin', true, true, true, true, true, false, false, false, true);
 --  login
 select id,
        email,
