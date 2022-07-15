7dbb8f088f66b946a0bf1f95ba15d459f578dcce
mahmoud reza kheyrati fard <64142359+mahmoudKheyrati@users.noreply.github.com>
2022-07-15 21:44:23 +0430

    update user passwords

--- deploy/database/data/add_users.sql
+++ deploy/database/data/add_users.sql
@@ -1,9 +1,9 @@
 INSERT INTO "user" (id,email,password,phone_number,first_name,last_name,permission_name)
-VALUES (1,'ali@project.com','ali123',9171234567,'ali','aliyari','normal-user'),
-(2,'reza@project.com','reza123',9161234567,'reza','rezaii','store-admin'),
-(3,'hosein@project.com','hosein123',9391234567,'hosein','mirhoseini','marketplace-admin'),
-(4,'mehdi@project.com','mehdi123',9177654321,'mehdi','mehdi','normal-user'),
-(5,'mahmoud@project.com','mahmoud123',9167654321,'mahmoud','kheyratifard','store-admin'),
-(6,'amir@project.com','amir123',9397654321,'amir','gheraati','marketplace-admin');
+VALUES (1,'ali@project.com','$2a$10$p7VeZwJIy/46H.ywPsfwnejrNaeFdAI38nz7W32rOaSXXnw1qxmvm',9171234567,'ali','aliyari','normal-user'),
+(2,'reza@project.com','$2a$10$6gVl/sKqzMM8n5fKZwF78OUhETzoa2WCrZZx8CSYRTQZQq8RhzFXK',9161234567,'reza','rezaii','store-admin'),
+(3,'hosein@project.com','$2a$10$WfzNdx56JirlcOCNhBbKbuszaqG7yB6NMO0Ie.0XCHa5WCNOZuk3G',9391234567,'hosein','mirhoseini','marketplace-admin'),
+(4,'mehdi@project.com','$2a$10$agJE/oPJdvt.R5f6RF8X0uibOyA9JW3oKYZIkPdbWkwQAr0GcvTLm',9177654321,'mehdi','mehdi','normal-user'),
+(5,'mahmoud@project.com','$2a$10$mUIc.uoApww5o4EQ3WlTs..zAov5QvZRIU/vmaDAMmIFfsW5dfdRW',9167654321,'mahmoud','kheyratifard','store-admin'),
+(6,'amir@project.com','$2a$10$h03yI0UHopUnMbYCYiRh8.wo02m5Qs6FrqVLDaNyDGK5ktED7laEy',9397654321,'amir','gheraati','marketplace-admin');
 
 
